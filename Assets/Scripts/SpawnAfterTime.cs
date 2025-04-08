ce40a4ac949ec87f5bc1d55655a8a329a32ac5a0
Markus L. Tanner <70599537+Darthsae@users.noreply.github.com>
2025-04-08 14:47:52 -0400

    Update SpawnAfterTime.cs

--- Assets/Scripts/SpawnAfterTime.cs
+++ Assets/Scripts/SpawnAfterTime.cs
@@ -4,13 +4,16 @@ namespace ASimpleRoguelike {
     public class SpawnAfterTime : MonoBehaviour {
         public float spawnTime = 10f;
         private float timer = 0f;
+        public GameObject prefabToSpawn;
+        public Vector2 area = new(0f, 0f);
+        public bool inheritRotation = false;
 
         private void Update() {
             if (GlobalGameData.isPaused) return;
             
             timer += Time.deltaTime;
             if (timer > spawnTime) {
-                
+                GameObject spawned = Instantiate(prefabToSpawn, transform.position + Spawner.InRange(area), inheritRotation ? transform.rotation : Quaternion.identity);
             }
         }
     }
