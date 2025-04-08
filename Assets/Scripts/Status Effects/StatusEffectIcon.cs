4cecc484fac08e077756fe0627d6e970172c326c
Markus L. Tanner <70599537+Darthsae@users.noreply.github.com>
2025-04-08 15:11:51 -0400

    Update StatusEffectIcon.cs

--- Assets/Scripts/Status Effects/StatusEffectIcon.cs
+++ Assets/Scripts/Status Effects/StatusEffectIcon.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 namespace ASimpleRoguelike.StatusEffects {
     public class StatusEffectIcon {
         public StatusEffect statusEffect;
-        public ;
+        public Image icon;
+
+        public void Init(StatusEffect statusEffect) {
+            this.statusEffect = statusEffect;
+            icon.sprite = statusEffect.icon;
+        }
     }
 }
 
